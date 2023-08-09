9d31877f0b9ddf8696542285dd6db1ec80378d0a
Oguz Ademoglu <oguzademoglu1@gmail.com>
2023-08-09 13:15:09 +0300

    bootstrap and react-bootstrap added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,48 @@
-// import { useState } from 'react'
-
-import './App.css'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { sampleProducts } from './data'
 import { Product } from './types/Product'
 
 function App() {
-  // const [count, setCount] = useState(10)
-
   return (
-    <>
-      <header>TS ECom</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>tscommerce</Navbar.Brand>
+            <Nav>
+              <a href="/cart" className="nav-link">
+                Cart
+              </a>
+              <a href="/signin" className="nav-link">
+                Sign In
+              </a>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {sampleProducts.map((product: Product) => (
-            <li key={product.slug}>
-              <img
-                className="product-image"
-                src={product.image}
-                alt={product.name}
-              />
-              <h2>
-                {product.brand} {product.name}
-              </h2>
-              <p>{product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product: Product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  className="product-image"
+                  src={product.image}
+                  alt={product.name}
+                />
+                <h2>
+                  {product.brand} {product.name}
+                </h2>
+                <p>{product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>All rights reserved.</footer>
-    </>
+      <footer>
+        <div className="text-center">All rights reserved.</div>
+      </footer>
+    </div>
   )
 }
 
